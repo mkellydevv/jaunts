07eff38b395806fae361b0704c854e22283da96f
mk <m.kelly.devv@gmail.com>
2021-06-12 00:14:37 -0400

    Added a star rating component to the review modal.  It will also work for displaying unchangable ratings

--- react-app/src/components/trail/ReviewModal.js
+++ react-app/src/components/trail/ReviewModal.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 
+import StarRating from "../random/StarRating";
+
 import "./ReviewModal.css"
 
 export default function ReviewModal({ trail }) {
+    const [rating, setRating] = useState(3);
 
     return (
         <>
             <div className="review-modal__name">{trail.name}</div>
-            <div>Stars</div>
             <div>
-
+                <StarRating fixed={false} setRating={setRating} rating={rating}/>
+            </div>
+            <div>
                 <input
                     className="review-modal__review"
                     type="textarea"
@@ -17,9 +21,15 @@ export default function ReviewModal({ trail }) {
                 />
             </div>
             <div>
-                <button className="review-modal__next">Next</button>
+                <span>Date</span>
+                <input
+                    type="date"
+                    placeholder="Start Date"
+                />
             </div>
-
+            <div>
+                <button className="review-modal__submit">Submit</button>
+            </div>
         </>
     )
 }
